0236b586ba29c18842fe8669d67e1dfbe495d41a
lollogiga <lorenzo.grande@hotmail.it>
2024-11-09 17:03:59 +0100

    DatasetPreprocessing.py --> We add preprocessing to dataset Next steps: 1) Initialize weight and bias; 2) Do backpropagation 3) Use SDG with momentum to find new weight 4) Evaluate model

---
+++ code/DatasetPreprocessing.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+
+
+def preprocess_data(df, label_column, test_size = 0.2, validation_size = 0.1):
+    """
+    Preprocessa i dati:
+    1) Separiamo features e labels.
+    2) Sostituisce i valori NaN con la media della colonna.
+    3) Esegue oversampling in caso di sbilanciamento delle classi.
+    4) Normalizza i dati.
+    5) Separiamo il dataset in training set, validation set, test set.
+
+    Parameters:
+        df (pandas.DataFrame): Il DataFrame contenente i dati.
+        label_column (str): Il nome della colonna della label (target).
+        training
+
+    Returns:
+        X_train, X_test, y_train, y_test: I dati preprocessati, separati in training e test set.
+        test_size (float): La percentuale di dati da usare per il test set (default 0.2).
+        validation_size (float): La percentuale di dati da usare per il validation set (default 0.1).
+    """
+    # 1) Dividi la label (y) dalle feature (X)
+    X = df.drop(columns=[label_column])
+    y = df[label_column]
+
+    # 2) Sostituisce i valori NaN con la media della rispettiva colonna
+    X = X.apply(lambda col: col.fillna(col.mean()), axis=0)
+
+    # 3) Esegui oversampling in caso di sbilanciamento delle classi
+    class_counts = y.value_counts()
+    if len(class_counts) > 1 and class_counts.max() > class_counts.min() * 2:
+        # Esegui oversampling solo se il bilanciamento delle classi è eccessivamente sbilanciato
+        X_resampled, y_resampled = resample(X, y,
+                                            replace=True,  # Campiona con sostituzione
+                                            n_samples=len(X),  # Campiona per avere la stessa quantità
+                                            random_state=42)
+        print("Oversampling eseguito.")
+        X, y = X_resampled, y_resampled
+
+    # 4) Normalizza i dati
+    scaler = StandardScaler()
+    X_normalized = scaler.fit_transform(X)
+
+    # 5) Separazione del dataset in training, validation e test set
+    # Prima dividi in training + test
+    X_train, X_test, y_train, y_test = train_test_split(X_normalized, y, test_size=test_size, random_state=42)
+
+    # Poi prendi una porzione di training per la validazione
+    validation_ratio = validation_size / (1 - test_size)  # Calcola la percentuale di validazione sul training
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=validation_ratio, random_state=42)
+
+    return X_train, X_valid, X_test, y_train, y_valid, y_test
+
---
+++ code/main.py
@@ -0,0 +1,79 @@
+import os
+from dbm import error
+
+import numpy as np
+import pandas as pd
+from DatasetPreprocessing import *
+
+
+def print_menu(message, choice_number):
+    """
+        Stampa a schermo una lista di opzioni
+
+        Parameters:
+            message: Messaggio da stampare a schermo
+            choice_number: Lista di scelte valide
+    """
+    while True:
+        print(message)
+        choice = input(f"Select a number between {choice_number}: ").strip().lower()
+        if choice in choice_number:
+            return choice
+        else:
+            print("Choice is not valid. Please select a valid input\n")
+
+
+def main():
+    dataset = print_menu(
+        "Choice a dateset:\n" +
+        "[1] Diabete_dataset\n"+
+        "[2] Decidere scelta2\n"+
+        "[3] Decidere scelta2\n",
+        ["1","2","3"]
+    )
+
+    activation_function = print_menu(
+        "What activation function do you want to use?\n"+
+        "[1] relu\n"+
+        "[2] tanh",
+        ["1", "2"]
+    )
+    if activation_function == "1":
+        activation_function = "relu"
+    elif activation_function == "2":
+        activation_function = "tanh"
+    else:
+        print("Error during define activation relu")
+        return -1
+
+    #Select dataset and preprocessing:
+    if dataset == "1":
+        dataset = pd.read_csv("../dataset/diabetes_dataset.csv")
+        print('Dataset shape: %s' % (str(dataset.shape)))
+        print("First 5 row:\n", dataset.head())
+        X_train, X_valid, X_test, y_train, y_valid, y_test = preprocess_data(dataset, "Diabetes_binary", 0.1, 0.2)
+    elif dataset == "2":
+        dataset = pd.read_csv("../dataset/")
+        print('Dataset shape: %s' % (str(dataset.shape)))
+        print("First 5 row:\n", dataset.head())
+        X_train, X_valid, X_test, y_train, y_valid, y_test = preprocess_data(dataset, "Diabetes_binary", 0.1, 0.2)
+    elif dataset == "3":
+        dataset = pd.read_csv("../dataset/")
+        print('Dataset shape: %s' % (str(dataset.shape)))
+        print("First 5 row:\n", dataset.head())
+        X_train, X_valid, X_test, y_train, y_valid, y_test = preprocess_data(dataset, "Diabetes_binary", 0.1, 0.2)
+    else:
+        print("Error during define dataset")
+        return -1
+
+    #TODO pulire precedenti test nelle cartelle di output
+    #Strategia Cross-Validation:
+    #Fissiamo una griglia di valori lambda:
+    # Creiamo una lista di valori lambda da testare
+    lambdaL1_values = np.logspace(-6, 6, 13)  # da 10^-6 a 10^6
+    lambdaL2_values = np.logspace(-6, 6, 13)  # da 10^-6 a 10^6
+
+    #Dobbiamo inizializzare i parametri:
+
+if __name__ == "__main__":
+    main()
